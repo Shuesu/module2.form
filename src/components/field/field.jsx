86baa7e25162c92ba6878114a91c37c8f33d096a
Atabayev Ali <sptmbrw@gmail.com>
2025-02-20 19:51:09 +0500

    finish

--- src/components/field/field.jsx
+++ src/components/field/field.jsx
@@ -1,55 +1,11 @@
-import { useEffect, useState } from 'react';
-import { validate } from './utils'
-import styles from './field.module.css'
+import { forwardRef } from 'react';
+import styles from './field.module.css';
 
-export const Field = ({
-   value,
-   setValue,
-   setIsValid,
-   validators,
-   dependencies = {},
-   forceValidation = () => false,
-   ...props
-}) => {
-   const [error, setError] = useState(null);
-   const [isDirty, setIsDirty] = useState();
-
-   const validateField = (currenValue, shouldValidate) => {
-      let error = null;
-      let isValid = false;
-
-      if (shouldValidate) {
-         error = validate(currenValue, validators);
-         isValid = error === null;
-      }
-
-      setError(error);
-      setIsValid(isValid);
-   };
-
-   useEffect(() => {
-      validateField(value, isDirty);
-   }, [...Object.values(dependencies)]);
-
-   const onChange = ({ target }) => {
-      setValue(target.value);
-      setIsDirty(true);
-
-      const isForceValidated = forceValidation(target.value);
-
-      validateField(target.value, isForceValidated);
-   };
-
-   const onBlur = () => validateField(value, isDirty);
-
+export const Field = forwardRef(({ error, ...props }, ref) => {
    return (
       <div>
-         <input
-            onChange={onChange}
-            onBlur={onBlur}
-            {...props}
-         />
+         <input ref={ref} {...props} />
          {error && <span className={styles.errorLabel}>{error}</span>}
-      </div >
+      </div>
    );
-}; 
+});
